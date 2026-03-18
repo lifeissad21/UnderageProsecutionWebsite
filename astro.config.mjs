169dce14796954c05f5c89315ddc9c9b0f2eaf0e
George Mathew <nerd2923@gmail.com>
2026-03-17 21:33:00 -0400

    Changed to conditional deployment to fix css error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+// Detect environment
+const isGitHub =
+  process.env.GITHUB_ACTIONS === 'true' ||
+  process.env.DEPLOY_TARGET === 'github';
+
 export default defineConfig({
-  site: "https://lifeissad21.github.io",
-  base: "/UnderageProsecution/",
+  site: isGitHub
+    ? "https://lifeissad21.github.io/UnderageProsecution"
+    : "https://underageprosecution.pages.dev",
+
+  base: isGitHub ? "/UnderageProsecution/" : "/",
+
   vite: {
     plugins: [tailwindcss()]
   }
